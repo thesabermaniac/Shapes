d814274c855ac36aedc56107be538eb5cf01c529
Shalom Vanderhoof <shalomzev@gmail.com>
2019-03-04 15:00:30 -0500

    Adding final variable for border

--- Rectangle.java
+++ Rectangle.java
@@ -5,6 +5,7 @@ public class Rectangle {
     char _f;
     int _length;
     int _height;
+    final int BORDER = 1;
     public Rectangle (char b, char f, int length, int height){
         this._b = b;
         this._f = f;
@@ -14,14 +15,14 @@ public class Rectangle {
     public String toString(){
         StringBuilder s = new StringBuilder();
         for (int i = 0; i < _height; ++i){
-            if (i == 0 || i == (_height -1)) {
+            if (i <= BORDER - 1 || i >= (_height - BORDER)) {
                 Line line = new Line(_b, _length);
                 s.append(line);
                 s.append('\n');
             }
             else{
                 for (int j = 0; j < _length; ++j){
-                    if (j == 0 || j == _length - 1){
+                    if (j <= (BORDER - 1) || j >= (_length - BORDER)){
                         s.append(_b);
                     }
                     else {
