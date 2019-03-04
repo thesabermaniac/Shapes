90f6e49cf8a8da5131546f7284f4459640fc718b
Shalom Vanderhoof <shalomzev@gmail.com>
2019-03-04 17:03:40 -0500

    Updating rectangle specs

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
         Line line = new Line('@', 10);
         System.out.println(line.toString());
         System.out.println('\n');
-        Rectangle rect = new Rectangle('@', '#', 10, 5);
+        Rectangle rect = new Rectangle('@', '#', 15, 25);
         System.out.println(rect.toString());
         System.out.println('\n');
         Square sq = new Square('@', '#', 5);
