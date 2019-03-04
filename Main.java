29020baaf5141231c3bcf92752d8a056819c80fa
Shalom Vanderhoof <shalomzev@gmail.com>
2019-03-04 14:52:23 -0500

    Committing source code for Main class

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        Line line = new Line('@', 10);
+        System.out.println(line.toString());
+        System.out.println('\n');
+        Rectangle rect = new Rectangle('@', '#', 10, 5);
+        System.out.println(rect.toString());
+        System.out.println('\n');
+        Square sq = new Square('@', '#', 5);
+        System.out.println(sq.toString());
+    }
+}
